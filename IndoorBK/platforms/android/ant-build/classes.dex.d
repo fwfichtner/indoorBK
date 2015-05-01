30a7230e052a9d62673ceba670f6c7434b509b37
ivodeliefde <ivodeliefde@gmail.com>
2015-05-01 20:38:41 +0200

    some more colour and style

--- IndoorBK/platforms/android/ant-build/classes.dex.d
+++ IndoorBK/platforms/android/ant-build/classes.dex.d
@@ -1,9 +1,9 @@
-D:\Data\Documents\GitHub\indoorBK\IndoorBK\platforms\android\ant-build\classes.dex : \
-D:\Data\Documents\GitHub\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\BuildConfig.class \
-D:\Data\Documents\GitHub\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\CordovaApp.class \
-D:\Data\Documents\GitHub\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R$attr.class \
-D:\Data\Documents\GitHub\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R$drawable.class \
-D:\Data\Documents\GitHub\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R$string.class \
-D:\Data\Documents\GitHub\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R$xml.class \
-D:\Data\Documents\GitHub\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R.class \
-D:\Data\Documents\GitHub\indoorBK\IndoorBK\platforms\android\ant-build\dexedLibs\classes-275f1f0ecb85c4fe4026b0728905c98e.jar \
+D:\indoorBK\IndoorBK\platforms\android\ant-build\classes.dex : \
+D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\BuildConfig.class \
+D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\CordovaApp.class \
+D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R$attr.class \
+D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R$drawable.class \
+D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R$string.class \
+D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R$xml.class \
+D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R.class \
+D:\indoorBK\IndoorBK\platforms\android\ant-build\dexedLibs\classes-4053d53a48dacb987386eac8db028554.jar \
--- IndoorBK/platforms/android/assets/www/main.js
+++ IndoorBK/platforms/android/assets/www/main.js
@@ -3,12 +3,3 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-angular.module('buttonsDemo1', ['ngMaterial'])
-.controller('AppCtrl', function($scope) {
-  $scope.title1 = 'Button';
-  $scope.title4 = 'Warn';
-  $scope.isDisabled = true;
-  $scope.googleUrl = 'http://google.com';
-});
-
