8db63af9401e5a8b4ace52a218986c2324280d4e
ivodeliefde <ivodeliefde@gmail.com>
2015-05-01 16:20:09 +0200

    set_up

---
+++ IndoorBK/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\NetbeansProjects\CordovaMapsSample\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+D:\NetbeansProjects\CordovaMapsSample\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ IndoorBK/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+D:\NetbeansProjects\CordovaMapsSample\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+D:\NetbeansProjects\CordovaMapsSample\platforms\android\ant-build\CordovaApp.ap_ \
+D:\NetbeansProjects\CordovaMapsSample\platforms\android\ant-build\classes.dex \
+D:\NetbeansProjects\CordovaMapsSample\platforms\android\CordovaLib\ant-build\classes.jar \
