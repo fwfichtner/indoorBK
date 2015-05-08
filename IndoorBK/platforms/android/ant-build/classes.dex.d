2ad63003084e029561a56da392315d78733877ec
dikkedoelie <tombroersen@hotmail.nl>
2015-05-08 16:31:58 +0200

    Revert "bla"
    This reverts commit 5d648c3be3210881f9f39a07ec75339f3c23d8cd.

--- IndoorBK/platforms/android/ant-build/classes.dex.d
+++ IndoorBK/platforms/android/ant-build/classes.dex.d
@@ -6,12 +6,4 @@ D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R
 D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R$string.class \
 D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R$xml.class \
 D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\coolappz\IndoorBK\R.class \
-D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\com\pylonproducts\wifiwizard\WifiWizard.class \
-D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\nl\xservices\plugins\accessor\AbstractCalendarAccessor$Attendee.class \
-D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\nl\xservices\plugins\accessor\AbstractCalendarAccessor$Event.class \
-D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\nl\xservices\plugins\accessor\AbstractCalendarAccessor$KeyIndex.class \
-D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\nl\xservices\plugins\accessor\AbstractCalendarAccessor.class \
-D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\nl\xservices\plugins\accessor\CalendarProviderAccessor.class \
-D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\nl\xservices\plugins\accessor\LegacyCalendarAccessor.class \
-D:\indoorBK\IndoorBK\platforms\android\ant-build\classes\nl\xservices\plugins\Calendar.class \
 D:\indoorBK\IndoorBK\platforms\android\ant-build\dexedLibs\classes-4053d53a48dacb987386eac8db028554.jar \
--- IndoorBK/platforms/android/assets/www/cordova_plugins.js
+++ IndoorBK/platforms/android/assets/www/cordova_plugins.js
@@ -1,29 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/com.pylonproducts.wifiwizard/www/WifiWizard.js",
-        "id": "com.pylonproducts.wifiwizard.DynamicUpdate",
-        "clobbers": [
-            "WifiWizard"
-        ]
-    },
-    {
-        "file": "plugins/nl.x-services.plugins.calendar/www/Calendar.js",
-        "id": "nl.x-services.plugins.calendar.Calendar",
-        "clobbers": [
-            "Calendar"
-        ]
-    },
-    {
-        "file": "plugins/nl.x-services.plugins.calendar/test/tests.js",
-        "id": "nl.x-services.plugins.calendar.tests"
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "com.pylonproducts.wifiwizard": "0.2.8",
-    "nl.x-services.plugins.calendar": "4.3.4"
-}
+{}
 // BOTTOM OF METADATA
 });
