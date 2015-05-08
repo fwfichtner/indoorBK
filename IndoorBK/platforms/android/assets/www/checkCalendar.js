423804bceb44f26cc290e5c86cb70681d61f8d1b
dikkedoelie <tombroersen@hotmail.nl>
2015-05-08 17:02:41 +0200

    Removed Calendar plugin

--- IndoorBK/platforms/android/assets/www/checkCalendar.js
+++ IndoorBK/platforms/android/assets/www/checkCalendar.js
@@ -7,81 +7,32 @@
 //This file checks the user's calendar and outputs the next appointment to 
 //index.html and to the server. 
 
-// prep some variables
-  var startDate = new Date(2015,2,15,18,30,0,0,0); // beware: month 0 = january, 11 = december
-  var endDate = new Date(2015,2,15,19,30,0,0,0);
-  var title = "My nice event";
-  var eventLocation = "Home";
-  var notes = "Some notes about this event.";
-  var success = function(message) { alert("Success: " + JSON.stringify(message)); };
-  var error = function(message) { alert("Error: " + message); };
+function checkCalendar (startDate, endDate) {
+    window.plugins.calendar.openCalendar();
+    // list all events in a date range (only supported on Android for now)
+    var success = function(message) { alert("Success: " + JSON.stringify(message)); };
+    var error = function(message) { alert("Error: " + message); };
+    var events = window.plugins.calendar.listEventsInRange(startDate,endDate,success,error);
+    alert(events);
+}
 
-  // create a calendar (iOS only for now)
-  window.plugins.calendar.createCalendar(calendarName,success,error);
-  // if you want to create a calendar with a specific color, pass in a JS object like this:
-  var createCalOptions = window.plugins.calendar.getCreateCalendarOptions();
-  createCalOptions.calendarName = "My Cal Name";
-  createCalOptions.calendarColor = "#FF0000"; // an optional hex color (with the # char), default is null, so the OS picks a color
-  window.plugins.calendar.createCalendar(createCalOptions,success,error);
+function checkDate(i) {
+    var today = new Date();
+    var dd = today.getDate() + i;
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
 
-  // delete a calendar (iOS only for now)
-  window.plugins.calendar.deleteCalendar(calendarName,success,error);
+    if(dd<10) {
+        dd='0'+dd
+    } 
 
-  // create an event silently (on Android < 4 an interactive dialog is shown)
-  window.plugins.calendar.createEvent(title,eventLocation,notes,startDate,endDate,success,error);
+    if(mm<10) {
+        mm='0'+mm
+    } 
 
-  // create an event silently (on Android < 4 an interactive dialog is shown which doesn't use this options) with options:
-  var calOptions = window.plugins.calendar.getCalendarOptions(); // grab the defaults
-  calOptions.firstReminderMinutes = 120; // default is 60, pass in null for no reminder (alarm)
-  calOptions.secondReminderMinutes = 5;
-
-  // Added these options in version 4.2.4:
-  calOptions.recurrence = "monthly"; // supported are: daily, weekly, monthly, yearly
-  calOptions.recurrenceEndDate = new Date(2015,10,1,0,0,0,0,0); // leave null to add events into infinity and beyond
-  calOptions.calendarName = "MyCreatedCalendar"; // iOS only
-  calOptions.calendarId = 1; // Android only, use id obtained from listCalendars() call which is described below. This will be ignored on iOS in favor of calendarName and vice versa. Default: 1.
-
-  // And the URL can be passed since 4.3.2 (will be appended to the notes on Android as there doesn't seem to be a sep field)
-  calOptions.url = "https://www.google.com";
-
-  window.plugins.calendar.createEventWithOptions(title,eventLocation,notes,startDate,endDate,calOptions,success,error);
-
-  // create an event interactively
-  window.plugins.calendar.createEventInteractively(title,eventLocation,notes,startDate,endDate,success,error);
-
-  // create an event interactively with the calOptions object as shown above
-  window.plugins.calendar.createEventInteractivelyWithOptions(title,eventLocation,notes,startDate,endDate,calOptions,success,error);
-
-  // create an event in a named calendar (iOS only for now)
-  window.plugins.calendar.createEventInNamedCalendar(title,eventLocation,notes,startDate,endDate,calendarName,success,error);
-
-  // find events (on iOS this includes a list of attendees (if any))
-  window.plugins.calendar.findEvent(title,eventLocation,notes,startDate,endDate,success,error);
-
-  // list all events in a date range (only supported on Android for now)
-  window.plugins.calendar.listEventsInRange(startDate,endDate,success,error);
-
-  // list all calendar names - returns this JS Object to the success callback: [{"id":"1", "name":"first"}, ..]
-  window.plugins.calendar.listCalendars(success,error);
-
-  // find all _future_ events in the first calendar with the specified name (iOS only for now, this includes a list of attendees (if any))
-  window.plugins.calendar.findAllEventsInNamedCalendar(calendarName,success,error);
-
-  // change an event (iOS only for now)
-  var newTitle = "New title!";
-  window.plugins.calendar.modifyEvent(title,eventLocation,notes,startDate,endDate,newTitle,eventLocation,notes,startDate,endDate,success,error);
-
-  // delete an event (you can pass nulls for irrelevant parameters, note that on Android `notes` is ignored). The dates are mandatory and represent a date range to delete events in.
-  // note that on iOS there is a bug where the timespan must not be larger than 4 years, see issue 102 for details.. call this method multiple times if need be
-  // since 4.3.0 you can match events starting with a prefix title, so if your event title is 'My app - cool event' then 'My app -' will match.
-  window.plugins.calendar.deleteEvent(newTitle,eventLocation,notes,startDate,endDate,success,error);
-
-  // delete an event, as above, but for a specific calendar (iOS only)
-  window.plugins.calendar.deleteEventFromNamedCalendar(newTitle,eventLocation,notes,startDate,endDate,calendarName,success,error);
-
-  // open the calendar app (added in 4.2.8):
-  // - open it at 'today'
-  window.plugins.calendar.openCalendar();
-  // - open at a specific date, here today + 3 days
-  var d = new Date(new Date().getTime() + 3*24*60*60*1000);
-  window.plugins.calendar.openCalendar(d, success, error); // callbacks are optional
+    today = mm+'/'+dd+'/'+yyyy;
+    alert(today);
+    
+    return today;
+}
+window.onload=checkCalendar(checkDate(0), checkDate(1));
