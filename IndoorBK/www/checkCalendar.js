5d0496ffa70669261ddb8b637e5ff47f9dc8fd83
ivodeliefde <ivodeliefde@gmail.com>
2015-05-11 12:46:13 +0200

    Major progress with getRSSi
    The getRSSi script basically loads the WifiWizard and starts using it.
    Just need to continue now with an actual Android Phone (the emulator
    doens't have a wifi connection to use for debuggin)

--- IndoorBK/www/checkCalendar.js
+++ IndoorBK/www/checkCalendar.js
@@ -7,27 +7,32 @@
 //This file checks the user's calendar and outputs the next appointment to 
 //index.html and to the server. 
 
-function checkCalendar (startDate, endDate) {
-    // list all events in a date range (only supported on Android for now)
-    var events = window.plugins.calendar.listEventsInRange(startDate,endDate,success,error);
-    alert(events);
-}
+// Call the print function to add text to the HTML div with the "nextAppoint" id in index.html
+var print = function(text) {
+        $("#nextAppoint").html(text);
+    }
 
-function checkDate(i) {
-    var today = new Date();
-    var dd = today.getDate() + i;
-    var mm = today.getMonth()+1; //January is 0!
-    var yyyy = today.getFullYear();
-
-    if(dd<10) {
-        dd='0'+dd
-    } 
-
-    if(mm<10) {
-        mm='0'+mm
-    } 
-
-    today = mm+'/'+dd+'/'+yyyy;
-    alert(today);
-}
-window.onload=checkCalendar(checkDate(0), checkDate(1));
+//function checkCalendar (startDate, endDate) {
+//    // list all events in a date range (only supported on Android for now)
+//    var events = window.plugins.calendar.listEventsInRange(startDate,endDate,success,error);
+//    //alert(events);
+//}
+//
+//function checkDate(i) {
+//    var today = new Date();
+//    var dd = today.getDate() + i;
+//    var mm = today.getMonth()+1; //January is 0!
+//    var yyyy = today.getFullYear();
+//
+//    if(dd<10) {
+//        dd='0'+dd
+//    } 
+//
+//    if(mm<10) {
+//        mm='0'+mm
+//    } 
+//
+//    today = mm+'/'+dd+'/'+yyyy;
+//    //alert(today);
+//}
+//window.onload=checkCalendar(checkDate(0), checkDate(1));
