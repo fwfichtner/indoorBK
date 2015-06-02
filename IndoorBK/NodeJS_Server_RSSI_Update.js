760f8a195a760846390c00db89649b83b93b71ba
dikkedoelie <tombroersen@hotmail.nl>
2015-06-02 17:27:59 +0200

    AutoUpdate
    Auto update of the route now works!

--- IndoorBK/NodeJS_Server_RSSI_Update.js
+++ IndoorBK/NodeJS_Server_RSSI_Update.js
@@ -1,15 +1,14 @@
 //Lets require/import the HTTP module
 var http = require('http');
 var FP = require('./NodeJS_Fingerprinting.js');
-var calendar = require('./NodeJS_get_calendar.js');
 var fs = require('fs');
 var pg = require('pg');
 
 
 //Lets define a port we want to listen to
-const PORT=8000;
+const PORT=8080;
 
-function calcRoute(startPoint, startLine, nextEvent, source, target, destPoint, destNode, destLine, response) {
+function calcRoute(startPoint, startLine, source, target, destPoint, response) {
     // Calculate the route 
     var conString = "postgres://postgres:Geomatics2015!@145.97.243.61:5432/postgres";
     var client = new pg.Client(conString);
@@ -18,6 +17,7 @@ function calcRoute(startPoint, startLine, nextEvent, source, target, destPoint, 
     var routeQuery = "SELECT "+SQL_ID+", ST_asGeoJSON(ST_Transform(ST_SetSRID(the_geom,3857),4326)) as route FROM ways WHERE "+SQL_ID+" in (SELECT id2 AS edge FROM pgr_dijkstra ('SELECT gid AS id, source::integer, target::integer, length::double precision AS cost FROM ways', "+source+", "+target+", false, false));";  
     var query1 = client.query(routeQuery);
     route = [];
+    nextEvent = [];
     
     query1.on('row', function(row) {
         route.push(row.route);
@@ -41,7 +41,6 @@ function calcRoute(startPoint, startLine, nextEvent, source, target, destPoint, 
             start_parsed = JSON.parse(startPoint);
             dest_parsed = JSON.parse(destPoint);
             startLine_parsed = JSON.parse(startLine);
-            destLine_parsed = JSON.parse(destLine);
  //           nextEvent[nextEvent.length - 1].coordinates.splice(0,0,dest_parsed.coordinates);
  //           nextEvent[3].coordinates.push(start_parsed.coordinates);
             
@@ -49,10 +48,8 @@ function calcRoute(startPoint, startLine, nextEvent, source, target, destPoint, 
             nextEvent.push(start_parsed);
             nextEvent.push(dest_parsed);
             nextEvent.push(startLine_parsed);
-            nextEvent.push(destLine_parsed);
-            nextEvent.push([destNode, target]);
 
-            // return the calendar and route
+            // return the updated route
             reply = JSON.stringify(nextEvent);
             response.write(reply);
 
@@ -63,34 +60,7 @@ function calcRoute(startPoint, startLine, nextEvent, source, target, destPoint, 
     });
 }
 
-function getEndlineGeom(startPoint, startLine, nextEvent, source, target, destPoint, destNode, destGid, response) {
-    console.log("finding startline geometry");
-    var conString = "postgres://postgres:Geomatics2015!@145.97.243.61:5432/postgres";
-    var client = new pg.Client(conString);
-    client.connect();
-    var startQuery = "SELECT ST_asGeoJSON(ST_Transform(ST_SetSRID(the_geom,3857),4326)) FROM ways WHERE gid = " + destGid + ";";  
-    var query2 = client.query(startQuery);
-    rows = [];
-    
-    query2.on('row', function(row) {
-        rows.push(row);
-    });
-
-    query2.on('end', function() { 
-        client.end();
-        if (rows.length == 0) {
-            console.log("Your geometry is not in the database!");
-        } else {
-            console.log("calculating route!");
-            destLine = rows[0].st_asgeojson;
-            calcRoute(startPoint, startLine, nextEvent, source, target, destPoint, destNode, destLine, response);
-        }
-    });
-}
-
-
-
-function getStartlineGeom(startGid, startPoint, nextEvent, source, target, destPoint, destNode, destGid, response) {
+function getStartlineGeom(startGid, startPoint, source, target, destPoint, response) {
     console.log("finding startline geometry");
     var conString = "postgres://postgres:Geomatics2015!@145.97.243.61:5432/postgres";
     var client = new pg.Client(conString);
@@ -110,13 +80,13 @@ function getStartlineGeom(startGid, startPoint, nextEvent, source, target, destP
         } else {
             console.log("calculating route!");
             startLine = rows[0].st_asgeojson;
-            getEndlineGeom(startPoint, startLine, nextEvent, source, target, destPoint, destNode, destGid, response);
+            calcRoute(startPoint, startLine, source, target, destPoint, response);
         }
     });
 }
 
 
-function getNodeGeom(position, startGid, nextEvent, source, target, destNode, destGid, response) {
+function getNodeGeom(position, startGid, source, target, destNode, response) {
     console.log("finding node geometry");
     var conString = "postgres://postgres:Geomatics2015!@145.97.243.61:5432/postgres";
     var client = new pg.Client(conString);
@@ -137,13 +107,13 @@ function getNodeGeom(position, startGid, nextEvent, source, target, destNode, de
             console.log("fetching line geometry!");
             startPoint = rows[0].st_asgeojson;
             destPoint = rows[1].st_asgeojson;
-            getStartlineGeom(startGid, startPoint, nextEvent, source, target, destPoint, destNode, destGid, response);
+            getStartlineGeom(startGid, startPoint, source, target, destPoint, response);
         }
     });
 }
 
 
-function findStartline(position, nextEvent, target, destNode, destGid, response) {
+function findStartline(position, target, destNode, response) {
     // Matches starting node to source
     console.log("finding startline");
     var conString = "postgres://postgres:Geomatics2015!@145.97.243.61:5432/postgres";
@@ -166,39 +136,10 @@ function findStartline(position, nextEvent, target, destNode, destGid, response)
             destination = "Your source is not in the database!";
         } else {
             console.log("calculating route!");
-            getNodeGeom(position, startGid, nextEvent, source, target, destNode, destGid, response);
+            getNodeGeom(position, startGid, source, target, destNode, response);
         }
     });
 }
-
-function findDestnode (position, nextEvent, nextLoc, response) {
-    var conString = "postgres://postgres:Geomatics2015!@145.97.243.61:5432/postgres";
-    var client = new pg.Client(conString);
-    client.connect();
-    var query_string = "SELECT node, target, gid FROM mappings WHERE room = '" + nextLoc + "';";  
-    var query = client.query(query_string);
-    rows = [];
-    console.log("first query done");
-    
-    query.on('row', function(row) {
-        rows.push(row); 
-        target = row.target;
-        destGid = row.gid;
-        destNode = row.node;
-    });
-
-    query.on('end', function() { 
-        client.end();
-        if (rows.length == 0) {
-            destination = "Your target is not in the database!";
-        } else { 
-            destination = "placeholder";
-            console.log("calling findline");
-            findStartline(position, nextEvent, target, destNode, destGid, response);
-        }
-    });
-}
-
 
 
 //We need a function which handles requests and send response
@@ -213,47 +154,22 @@ function handleRequest(request, response){
     request.on("end", function() {
         console.log("All data received");
         console.log(RSSI);
+
+        destNode = RSSI[RSSI.length - 2];
+        target = RSSI[RSSI.length - 1];
         
         // Find the location of the user via WiFi Fingerprinting   
-        FP.getFingerprints(RSSI, function (position, err) {
+        FP.getFingerprints(RSSI.slice(0,RSSI.length - 2), function (position, err) {
             if (err) throw err;
-            
-            // Get the next event from the calendar
-            
-            calendar.getCalendar(function callback(err, nextEvent){
-                if (err) throw err;
-                
- //               nextEvent = "13:00#SomeClass#BK-IZ R"
-                nextEvent = nextEvent.split("#");
-
-                // Parse Google Calendar API time to Javascript time object in milliseconds
-                var nextTime = Date.parse(nextEvent[0]);
-                nextEvent[0] = nextTime;
-
-                // Matches the Calendar location to destination node
-                if (RSSI[RSSI.length - 1] == "BK-IZ R") {
-                    var nextLoc = "BK-IZ R";
-                    nextEvent[2] = nextLoc;
-                } else {
-                    var nextLoc = nextEvent[2];
-                }              
-
+               
                 if (position == -1) {
-                    nextEvent.push(position);
-                    response.write(JSON.stringify(nextEvent));
+                    reply = [position];
+                    response.write(JSON.stringify(reply));
                     console.log("The location is not in BK!");
                     response.end();                    
-                } else if (["BK-IZ R","BK-IZ Z","BK-IZ V","Bouwpub","Geolab"].indexOf(nextLoc) !== -1) {
-                    findDestnode(position, nextEvent, nextLoc, response);
                 } else {
-                    response.write(JSON.stringify(nextEvent));
-                    console.log("The destination is not in BK!");
-                    response.end();
+                    findStartline(position, target, destNode, response);
                 }
-
-            });
-            
-            
         });   
   	});
 }
--- IndoorBK/www/getRSSi.js
+++ IndoorBK/www/getRSSi.js
@@ -29,13 +29,14 @@ var print = function(text) {
 
 // This function adds GeoJSON to map
 var addGeoJSON = function(list){
-    // THis part should remove any previous route layers, but it doesn't work yet
-//    map.eachLayer(function(layer){
-//        if (layer._leaflet_id === "1"){
-//            map.removeLayer(layer);
-//	};
-//    });
-    
+
+    //THis part should remove any previous route layers, but it doesn't work yet
+    // map.eachLayer(function(layer){
+    //     if (layer._leaflet_id == test[0]){
+    //         map.removeLayer(layer);
+    //     };
+    // });
+
     // Make the new layer 
     route = new L.GeoJSON();
     // Add the individual GeoJSON linestrings to the layer
@@ -44,55 +45,36 @@ var addGeoJSON = function(list){
     });
     // Set some additional styling
     route.setStyle({color:'red'});
+
     // add the layer to the map
     route.addTo(map);
     // zoom to layer
  //   map.fitBounds(route.getBounds());
     // bring the layer on top of the floorplan
     route.bringToFront();
+    
+    // Shows navigation button once ready
+    $("#Loading").hide();
+    $("#Navigate").show();
+
 };
+
+
 
 // Error callback function -- displays error message in alert
 var fail = function (err) {
     alert("error: "+err);
-    //print("Ajax fail");
 };
 
-// This is the main function that calls the server as well
-function getRSSi(){
 
-    // Show and hide specific divs
-    $("#Welcome").show();
-    $("#pageone").hide();
-    $("#Navigate").hide();
-    $("#ToStart").hide();
-    $("#map").hide();
-//     these below are for debugging:
-//        $("#nextAppoint").hide();
-//        $("#Loading").hide();
 
-    // // When welcome page is clicked it is replaced by the main page
-    // $("#Welcome").on("click", function(){
-    //     $("#pageone").show();
-    //     $("#Welcome").hide();
-    // });
 
-    // As long as we're waiting for the server data we ask for some patience
-    $("#Loading").on("click", function(){
-        alert("Just a second, almost ready!");
-    });
+function update(destNode, target) {
 
-    // When the navigate button is clicked the map is shown 
-    // and the 'back to start' button is loaded.
-    $("#Navigate").on("click", function(){
-        $("#Navigate").hide();
-        $("#ToStart").show();
-        $("#nextAppoint").hide();
-        $("#map").show();
-    }); 
+    // Retrieves the updated RSSI values and executes listHandlerAuto
+    WifiWizard.getScanResults({numLevels: false}, listHandlerAuto, fail);
 
-    // Check the results of the getScanResults
-    var listHandler = function (list) {      
+    function listHandlerAuto (list) {      
 
         // If more than 5 access points are available, this if-else statement finds
         // the 5 with strongest signal level and stores them in a new array
@@ -121,11 +103,11 @@ function getRSSi(){
 
         //Dummy RSSi values
         var RSSI = [
-            { level : -58.0, SSID: "tudelft-dastud", BSSID : "00-22-90-5E-69-21" },
-            { level : -58.0, SSID: "TUvisitor", BSSID : "00-22-90-38-AE-40" },
-            { level : -57.0, SSID: "tudelft-dastud", BSSID : "00-22-90-5E-69-20" },
-            { level : -57.0, SSID: "TUvisitor", BSSID : "00-22-90-38-AE-41" },
-            { level : -56.0, SSID: "TUvisitor", BSSID : "00:22:90-38-AE-42" }
+            { level : -45.0, SSID: "TUvisitor", BSSID : "00-22-90-38-98-51" },
+            { level : -45.0, SSID: "tudelft-dastud", BSSID : "00-22-90-38-98-52" },
+            { level : -45.0, SSID: "eduroam", BSSID : "00-22-90-38-98-50" },
+            { level : -59.0, SSID: "eduroam", BSSID : "00-22-90-5E-63-20" },
+            { level : -60.0, SSID: "TUvisitor", BSSID : "00-22-90-38-98-5E" }
             ];
 
         // Converts the BSSID to capital letters
@@ -134,64 +116,199 @@ function getRSSi(){
         listObjects[i].BSSID = listObjects[i].BSSID.replace(/:/g,"-");
         }
 
+        // Useful for debugging: Gives you the choice to use RSSI measurements, or dummy values
         if (confirm("Press 'OK' to use RSSI measurements, or press 'cancel' to use dummy values")) {
-            connectServer(listObjects);
+            listObjects.push(destNode);
+            listObjects.push(target);
+            connectServerAuto(listObjects);
         } else {
-            connectServer(RSSI);
+            RSSI.push(destNode);
+            RSSI.push(target);
+            connectServerAuto(RSSI);
         }
-       
+    }
 
-        // Calls the server and sends the RSSI readings.
-        function connectServer(list) {
-            $.ajax({
-            url: 'http://145.97.243.61:8000',
-            data: JSON.stringify(list),
-            contentType: 'application/json',
-            type: 'POST',      
-            success: function (data) {
-                data = JSON.parse(data);
-                printAppoint(data.slice(0,3));
-                $("#Welcome").hide();
-                $("#pageone").show();
-                $("#Loading").hide();
-                $("#Navigate").show();
+    function connectServerAuto(list) {
+        $.ajax({
+        url: 'http://145.97.243.61:8080',
+        data: JSON.stringify(list),
+        contentType: 'application/json',
+        type: 'POST',      
+        success: function (data) {
+            data = JSON.parse(data);
 
-                if (data.length < 4) {
-                    alert("Your destination is not in BK!");
-                    if (confirm("Do you want to use a dummy destination?")) {
-                        $("#Welcome").show();
-                        $("#pageone").hide();
-                        list.push("BK-IZ R");
-                        connectServer(list);
-                    } else {
-                        alert("No route could be determined!");
-                    }
-                } else if (data[3] == -1) {
-                    alert("Your current location is not in BK!");
-                    if (confirm("Do you want to try again?")) {
-                        $("#Welcome").show();
-                        $("#pageone").hide();
-                        getRSSi();
-                    } else {
-                        alert("No route could be determined!");
-                    }
-                } else {
-                    addGeoJSON(data.slice(3));
-                }
-                
-            },
-                // When ajax fails the error message is shown as an alert
-                error: function (xhr, status, error) {
-                    alert('Error: an error occurred in the server!');
-                    if (confirm("Do you want to try again?")) {
-                        connectServer(list);
-                    } else {
-                        alert("Application stopped");
-                    }
-                }
-            });
+            if (data[0] == -1) {
+                alert("Your current location is not in BK!");       
+            } else {
+                addGeoJSON(data);
+            }
+            
+        },
+            // When ajax fails the error message is shown as an alert
+            error: function (xhr, status, error) {
+                alert('Error: an error occurred in the server!');
+            }
+        });
+    }
+
+
+}
+
+
+
+// Calls the server and sends the RSSI readings.
+function connectServer(list) {
+    $.ajax({
+    url: 'http://145.97.243.61:8000',
+    data: JSON.stringify(list),
+    contentType: 'application/json',
+    type: 'POST',      
+    success: function (data) {
+        data = JSON.parse(data);
+        printAppoint(data.slice(0,3));
+        $("#Welcome").hide();
+        $("#pageone").show();
+
+        if (data.length < 4) {
+            alert("Your destination is not in BK!");
+            if (confirm("Do you want to use a dummy destination?")) {
+                $("#Welcome").show();
+                $("#pageone").hide();
+                list.push("BK-IZ R");
+                connectServer(list);
+            } else {
+                alert("No route could be determined!");
+            }
+        } else if (data[3] == -1) {
+            alert("Your current location is not in BK!");
+            if (confirm("Do you want to try again?")) {
+                $("#Welcome").show();
+                $("#pageone").hide();
+                getRSSi();
+            } else {
+                alert("No route could be determined!");
+            }
+        } else {
+            addGeoJSON(data.slice(3,data.length - 1));
+            var newdata = data[data.length - 1];
+            destNode = newdata[0];
+            target = newdata[1];
+
+
         }
-    };
+        
+    },
+        // When ajax fails the error message is shown as an alert
+        error: function (xhr, status, error) {
+            alert('Error: an error occurred in the server!');
+            if (confirm("Do you want to try again?")) {
+                connectServer(list);
+            } else {
+                alert("Application stopped");
+            }
+        }
+    });
+}
+
+
+
+// Check the results of the getScanResults
+function listHandler (list) {      
+
+    // If more than 5 access points are available, this if-else statement finds
+    // the 5 with strongest signal level and stores them in a new array
+    if (list.length > 5) {
+
+        // lists all rssi-levels and finds the fifth-largest value
+        var listLevels = Array(); 
+        for (var i = 0; i < list.length; i++) {
+            listLevels.push(list[i].level);
+        }
+        listLevels.sort(function(a,b){return b-a});
+        threshValue = listLevels[4];
+
+        // finds all objects with rssi-levels above the fifth-largest value
+        // and stores them in a new array
+        var listObjects = Array();
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].level >= threshValue) {
+                listObjects.push(list[i]);
+            }
+        }
+
+    } else {
+        var listObjects = list;
+    }
+
+    //Dummy RSSi values
+    var RSSI = [
+        { level : -58.0, SSID: "tudelft-dastud", BSSID : "00-22-90-5E-69-21" },
+        { level : -58.0, SSID: "TUvisitor", BSSID : "00-22-90-38-AE-40" },
+        { level : -57.0, SSID: "tudelft-dastud", BSSID : "00-22-90-5E-69-20" },
+        { level : -57.0, SSID: "TUvisitor", BSSID : "00-22-90-38-AE-41" },
+        { level : -56.0, SSID: "TUvisitor", BSSID : "00:22:90-38-AE-42" }
+        ];
+
+    // Converts the BSSID to capital letters
+    for (i = 0; i < listObjects.length; i++){
+    listObjects[i].BSSID = listObjects[i].BSSID.toUpperCase();
+    listObjects[i].BSSID = listObjects[i].BSSID.replace(/:/g,"-");
+    }
+
+    // Useful for debugging: Gives you the choice to use RSSI measurements, or dummy values
+    if (confirm("Press 'OK' to use RSSI measurements, or press 'cancel' to use dummy values")) {
+        connectServer(listObjects);
+    } else {
+        connectServer(RSSI);
+    }
+}
+
+
+
+
+
+// This is the main function that calls the server as well
+function getRSSi(){
+
+    // Show and hide specific divs
+    $("#Welcome").show();
+    $("#pageone").hide();
+    $("#Navigate").hide();
+    $("#ToStart").hide();
+    $("#map").hide();
+
+    // // When welcome page is clicked it is replaced by the main page
+    // $("#Welcome").on("click", function(){
+    //     $("#pageone").show();
+    //     $("#Welcome").hide();
+    // });
+
+    // As long as we're waiting for the server data we ask for some patience
+    $("#Loading").on("click", function(){
+        alert("Just a second, almost ready!");
+    });
+
+    // When the navigate button is clicked the map is shown 
+    // and the 'back to start' button is loaded.
+    $("#Navigate").on("click", function(){
+        $("#Navigate").hide();
+        $("#ToStart").show();
+        $("#nextAppoint").hide();
+        $("#map").show();
+        
+        // execute autoUpdate function after every 5 seconds
+        window.setTimeout(autoUpdate, 10000);
+        var autoUpdate = setInterval(function () {update(destNode, target)}, 10000);
+
+    }); 
+
+    // Implements restart button in map display
+    $("#ToStart").on("click", function(){
+        $("#ToStart").hide();
+        $("#map").hide();
+        $("#Welcome").show();
+        location.reload(); 
+    }); 
     
     // Retrieves the RSSI values
     WifiWizard.getScanResults({numLevels: false}, listHandler, fail);
